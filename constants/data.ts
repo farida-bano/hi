4a72fb761544c35b4bcb96f1c1af0b9255e577e7
farida <bano23086@gmaile.com>
2025-05-16 20:49:27 +0500

    Resolve merge conflicts

---
+++ constants/data.ts
@@ -0,0 +1,35 @@
+export const headerData = [
+     { title: "Home", href: "/" },
+     { title: "Shop", href: "/shop" },
+     { title: "Blog", href: "/blog" },
+     { title: "Hot Deal", href: "/deal" },
+     //   { title: "Contact", href: "/contact" },
+   ];
+   export const quickLinksData = [
+     { title: "About us", href: "/about" },
+     { title: "Contact us", href: "/contact" },
+     { title: "Terms & Conditions", href: "/terms" },
+     { title: "Privacy Policy", href: "/privacy" },
+     { title: "FAQs", href: "/faqs" },
+     { title: "Help", href: "/help" },
+   ];
+   export const categoriesData = [
+      { title: "Food Items (Crochet)", href: "food-items" },
+      { title: "Fruits (Crochet)", href: "fruits" },
+      { title: "Vegetables (Crochet)", href: "vegetables" },
+      { title: "Wall Hangings", href: "wall-hangings" },
+      { title: "Table Mats", href: "table-mats" },
+      { title: "Other Handmade Items", href: "handmade-items" },
+      { title: "Crochet Hair Accessories", href: "crochet hair accessories" },
+      { title: "Crochet-Bags", href: "crochet-bags" },
+      { title: "Crochet-Keychains", href: "crochet-keychains" },
+      { title: "Crochet Cake Biscuits Ice Verity", href: "crochet cake biscuits ice verity" },
+      { title: "Yarn and Tools", href: "yarn and tools" }
+    ];
+    export const productType = [
+      { title: "Decorative", value: "decorative" },
+      { title: "Toys", value: "toys" },
+      { title: "Home Use", value: "home-use" },
+      { title: "Gift Items", value: "gift-items" },
+      { title: "Others", value: "others" }
+    ];
